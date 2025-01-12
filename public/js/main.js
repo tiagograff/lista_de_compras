cc0822b1617674c2a4b02ba6ab0012d71ae66641
tiago <tiagopinzgraffunder@gmail.com>
2025-01-12 20:02:12 -0300

    feat: adicionando modal e estilizando

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,7 @@
+const intensList = [];
 const root = document.documentElement;
+
+//lista
 const sectionShoppingList = document.querySelector(".shopping-list");
 const sectionComplete = document.querySelector(".complete-purchase");
 const buttonComplete = document.querySelector(".complete-purchase button");
@@ -14,3 +17,44 @@ if (!sectionShoppingList.innerHTML.trim()) {
 } else {
   sectionComplete.style.display = "block";
 }
+
+//modal
+const addButton = document.getElementById("add-button");
+const modal = document.getElementById("modal");
+const closeModalButton = document.getElementById("close-modal");
+const form = document.getElementById("modal-form");
+
+addButton.addEventListener("click", () => {
+  modal.style.display = "flex";
+  modal.setAttribute("aria-hidden", "false");
+});
+
+closeModalButton.addEventListener("click", () => {
+  modal.style.display = "none";
+  modal.setAttribute("aria-hidden", "true");
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+    modal.setAttribute("aria-hidden", "true");
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const itemName = document.getElementById("item-name").value;
+  if (itemName.trim()) {
+    const addItemInList = confirm(
+      `Tem certeza que deseja adicionar? ${itemName}`
+    );
+    addItemInList === true
+      ? intensList.push(itemName)
+      : console.log("item não adicionado!");
+  }
+
+  modal.style.display = "none";
+  modal.setAttribute("aria-hidden", "true");
+  form.reset();
+});
